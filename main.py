4e7cce6b387b130f079d6239f9431e67e633b22e
diyaanne <diyaanne9@gmail.com>
2025-07-30 16:18:42 +0000

    code for featurization

---
+++ main.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from tensorflow import keras
+import joblib
+
+IMG_SIZE = (224, 224)
+
+
+# Download the model, valid alpha values [0.25,0.35,0.5,0.75,1]
+base_model = keras.applications.MobileNetV2(
+    include_top=False,
+    weights="imagenet",
+    input_shape=(224, 224, 3),
+    pooling='avg'
+)
+
+
+# base_model = tf.keras.applications.MobileNetV2(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
+# Add average pooling to the base
+x = base_model.output
+model_frozen = Model(inputs=base_model.input,outputs=x)
+
+# Load the model from a .pkl file
+model = joblib.load('MLP_best_model.pkl')
+
+def convert_tf_dataset(img_path, model):
+    # This function passes all images provided in PATH
+    # and passes them through the model.
+    # The result is a featurized image along with labels
+
+    IMG_SIZE = (224, 224)
+  
+      
+    img = tf.keras.preprocessing.image.load_img(img_path, target_size=IMG_SIZE)
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_batch = np.expand_dims(img_array, axis=0)
+    img_preprocessed = preprocess_input(img_batch)
+    data = [model.predict(img_preprocessed, verbose=False)]
+
+    return data
+
